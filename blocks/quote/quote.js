3775bf53848bb11517f5f44e98a5ff38d2f0b155
git user id <git account email>
2025-06-27 14:13:18 +0530

    Change

--- blocks/quote/quote.js
+++ blocks/quote/quote.js
@@ -1,7 +1,6 @@
 import { getConfigValue } from '../../scripts/configs.js';
 
 export default async function decorate(block) {
-
   const readConfig = await getConfigValue('get-value');
   const [quoteWrapper] = block.children;
   const blockquote = document.createElement('blockquote');
